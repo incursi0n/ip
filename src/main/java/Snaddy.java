b7b1d1f515d83a6507035076bb7863a2dd4a2de2
incursi0n <zhengjiawen02@gmail.com>
2026-01-22 12:49:51 +0800

    Rename chatbot and add greeting

---
+++ src/main/java/Snaddy.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Snaddy {
+    public static void main(String[] args) {
+        String divider = "      ____________________________________________________________\n";
+        String logo = divider
+                + "       _________                  .___  .___      \n"
+                + "      /   _____/ ____ _____     __| _/__| _/__.__.\n"
+                + "      \\_____  \\ /    \\\\__  \\   / __ |/ __ <   |  |\n"
+                + "       /        \\   |  \\/ __ \\_/ /_/ / /_/ |\\___  |\n"
+                + "       /_______  /___|  (____  /\\____ \\____ |/ ____|\n"
+                + "               \\/     \\/     \\/      \\/    \\/\\/     \n";
+        String goodbye = "      Bye. Hope to see you again soon!\n";
+
+        System.out.print(logo + "      Hello! I'm Snaddy\n      What can I do for you?\n" + divider + goodbye);
+    }
+}
